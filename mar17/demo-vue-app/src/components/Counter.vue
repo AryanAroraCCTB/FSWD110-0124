a103f1c8e7f214b2470c30c27babdc0290dd7596
Aryan Arora <aryan.arora@canctb.ca>
2025-03-17 19:34:23 -0700

    Auto commit

---
+++ mar17/demo-vue-app/src/components/Counter.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const count = ref(3); // same as useState(3) in react
+
+const state = reactive({
+    count: 5,
+    greeting: "Good Evening!",
+    fruits: ["apple", "banana", "cherry"],
+    isActive: true,
+    authInfo: {
+        fname: "Fname",
+        lname: "Lname",
+        age: 11,
+    },
+});
+
+function toggleStatus() {
+    state.isActive = !state.isActive;
+}
+</script>
+
+<template>
+    <div>
+        <h1 class="heading">{{ state.greeting }}</h1>
+        <p>Counter: {{ count }}</p>
+        <p>Counter 2: {{ state.count }}</p>
+
+        <p>{{ state.isActive ? "Working" : "Out of Order" }}</p>
+        <button @click="toggleStatus">Toggle Status</button>
+
+        <hr />
+
+        <p>{{ state.authInfo.fname }}</p>
+        <p>{{ state.authInfo.lname }}</p>
+
+        <ul>
+            {{
+                state.fruits.map((fruit, index) => {})
+            }}
+        </ul>
+    </div>
+</template>
+
+<style>
+.heading {
+    color: brown;
+}
+</style>
--- mar17/demo-vue-app/src/components/HelloWorld.vue
+++ mar17/demo-vue-app/src/components/HelloWorld.vue
@@ -6,13 +6,17 @@ defineProps({
 });
 
 const count = ref(0); // similar to useState in react
+
+function increment() {
+    count.value += 1;
+}
 </script>
 
 <template>
     <h1>{{ msg }}</h1>
 
     <div class="card">
-        <button type="button" @click="count++">count is {{ count }}</button>
+        <button type="button" @click="increment">count is {{ count }}</button>
         <p>
             Edit
             <code>components/HelloWorld.vue</code> to test HMR
